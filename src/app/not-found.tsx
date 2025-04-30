684b36bcd9884349b0fdcccceeaed3b74186b359
lluz <luisluz@gmail.com>
2025-04-30 18:56:24 +0100

    Added a benign not-found.tsx to folder app

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+
+  console.log('----------------- NotFoundPage -----------------');
+
+  return <>----------------- NotFoundPage -----------------</>;
+  
+}
